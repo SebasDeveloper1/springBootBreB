f4f9407328e01b7b089fe04a9741aae0971e49ee
Sebastian-Pedroza <sebaspedroza.developer@gmail.com>
2024-11-13 15:54:07 -0500

    🚀 Inicializa el proyecto con configuración base

---
+++ src/main/java/co/com/semillero/springBootBreB/SpringBootBreBApplication.java
@@ -0,0 +1,13 @@
+package co.com.semillero.springBootBreB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBreBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBreBApplication.class, args);
+	}
+
+}
